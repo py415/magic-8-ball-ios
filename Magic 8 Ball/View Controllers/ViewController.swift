efc705a8b006914b57f9e14ca3b79fbb38cd1924
Philip Yu <pyu3@mail.sfsu.edu>
2019-06-28 13:26:57 -0700

    Added 8-Ball response randomization

--- Magic 8 Ball/View Controllers/ViewController.swift
+++ Magic 8 Ball/View Controllers/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Properties
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber: Int = 0
+    
+    // Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        newBallImage()
+        
     }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        
+        newBallImage()
+        
+    }
+    
+    func newBallImage() {
+        
+        randomBallNumber = Int.random(in: 0 ... 4)
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+        
+    }
+    
 }
 
