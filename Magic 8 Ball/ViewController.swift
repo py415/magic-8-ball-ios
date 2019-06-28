cf1d68ce1604126ebff21ccbee594878177790b0
Philip Yu <pyu3@mail.sfsu.edu>
2019-06-28 13:08:16 -0700

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Philip Yu on 6/28/19.
+//  Copyright © 2019 Philip Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
